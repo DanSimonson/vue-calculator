8c42333a7d876dea10a2b5d3c5c58133a4ddf658
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2019-01-06 03:26:41 +0800

    first draft of logic/user stories completed

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="wrap">
     <div class="calculator">
-      <div id="display" class="btn">0.6523</div>
-      <div id="clear" class="btn">C</div>
-      <div id="sign" class="btn">+/-</div>
-      <div id="percent" class="btn">%</div>
-      <div id="divide" class="btn operator">&#247</div>
-      <div id="seven" class="btn">7</div>
-      <div id="eight" class="btn">8</div>
-      <div id="nine" class="btn">9</div>
-      <div id="multiply" class="btn operator">x</div>
-      <div id="four" class="btn">4</div>
-      <div id="five" class="btn">5</div>
-      <div id="six" class="btn">6</div>
-      <div id="subtract" class="btn operator">-</div>
-      <div id="one" class="btn">1</div>
-      <div id="two" class="btn">2</div>
-      <div id="three" class="btn">3</div>
-      <div id="add" class="btn operator">+</div>
-      <div id="zero" class="btn">0</div>
-      <div id="decimal" class="btn">.</div>
-      <div id="equals" class="btn operator">=</div>
+      <div id="display" class="btn">{{result || 0}}</div>
+      <div @click="clear()" id="clear" class="btn">C</div>
+      <!--<div @click="number('1')" id="sign" class="btn">+/-</div>
+      <div @click="unique('%')" id="percent" class="btn">%</div>-->
+      <div @click="math('/' )" id="divide" class="btn operator">&#247</div>
+      <div @click="number('7')" id="seven" class="btn">7</div>
+      <div @click="number('8')" id="eight" class="btn">8</div>
+      <div @click="number('9')" id="nine" class="btn">9</div>
+      <div @click="math('*')" id="multiply" class="btn operator">&#215;</div>
+      <div @click="number('4')" id="four" class="btn">4</div>
+      <div @click="number('5')" id="five" class="btn">5</div>
+      <div @click="number('6')" id="six" class="btn">6</div>
+      <div @click="math('-')" id="subtract" class="btn operator">&#8722;</div>
+      <div @click="number('1')" id="one" class="btn">1</div>
+      <div @click="number('2')" id="two" class="btn">2</div>
+      <div @click="number('3')" id="three" class="btn">3</div>
+      <div @click="math('+')" id="add" class="btn operator">&#43;</div>
+      <div @click="number('0')" id="zero" class="btn">0</div>
+      <div @click="unique('.')" id="decimal" class="btn">.</div>
+      <div @click="equal" id="equals" class="btn operator">=</div>
     </div>
   </div>
 </template>
@@ -30,6 +30,52 @@ export default {
   name: "Calculator",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      result: "",
+      results: []
+    };
+  },
+  methods: {
+    number(item) {
+      //number not allowed to start with multiple zeros
+      if (item === "0" && this.result.length < 2) {
+        this.result = "0";
+      } else {
+        this.result += item;
+      }
+    },
+    math(operator) {
+      //get last char in data string
+      const lastChar = this.result[this.result.length - 1];
+      //check if last character is an operand
+      const bool = ["*", "+", "-", "/"].includes(lastChar);
+      if (bool) {
+        //remove last char since it is operand
+        this.result = this.result.slice(0, -1);
+        //replace with new operand
+        this.result += operator.toString();
+      } else {
+        //just append operand
+        this.result += operator.toString();
+      }
+    },
+    unique(operator) {
+      //two '.' operators will not be accepted in one number
+      if (this.result.includes(".") === false) {
+        this.result += operator.toString();
+      }
+    },
+    equal() {
+      let result = this.result;
+      let total = math.eval(result);
+      this.results.push({ math: result, total: result });
+      this.result = total;
+    },
+    clear() {
+      this.result = "";
+    }
   }
 };
 </script>
@@ -73,6 +119,10 @@ export default {
 }
 #zero {
   grid-column: 1/3;
+  text-align: center;
+}
+#clear {
+  grid-column: 1/4;
   text-align: center;
 }
 .operator {
